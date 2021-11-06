7fa79894c77d3236b1cef488acde220593113c0b
Gabriel Poça <gabrielpoca@users.noreply.github.com>
2021-11-06 16:04:31 +0000

    Create an option to disable renaming/moving files (#8)

--- lua/replacer/init.lua
+++ lua/replacer/init.lua
@@ -22,7 +22,14 @@ function table.empty(self)
   return true
 end
 
-function replacer.run()
+function replacer.run(opts)
+  local opts = opts or {}
+  local rename_files =  true
+
+  if opts['rename_files'] ~= nil and opts['rename_files'] == false then
+    rename_files = false
+  end
+
   local bufnr = vim.fn.bufnr()
 
   if vim.api.nvim_buf_get_option(vim.fn.bufnr(), "filetype") ~= "qf" then
@@ -42,16 +49,20 @@ function replacer.run()
     vim.api.nvim_buf_set_lines(bufnr, i - 1, i - 1, false, {line})
   end
 
-  local write_autocmd = string.format('autocmd BufWriteCmd <buffer=%s> lua require"replacer".save(%s)', bufnr, bufnr)
+  local write_autocmd = string.format('autocmd BufWriteCmd <buffer=%s> lua require"replacer".save(%s, { rename_files = %s })', bufnr, bufnr, rename_files)
+
   vim.api.nvim_command(write_autocmd)
 
   vim.cmd('setlocal nocursorcolumn nonumber norelativenumber')
   api.nvim_buf_set_name(bufnr, 'replacer://' .. bufnr)
   api.nvim_buf_set_option(bufnr, 'buftype', 'acwrite')
   api.nvim_buf_set_option(bufnr, 'filetype', 'replacer')
+  api.nvim_buf_set_option(qf_bufnr, "modified", false)
 end
 
-function replacer.save(qf_bufnr)
+function replacer.save(qf_bufnr, opts)
+  local rename_files = opts['rename_files']
+
   local qf_win_nr = vim.fn.bufwinid(qf_bufnr)
 
   vim.api.nvim_buf_set_option(qf_bufnr, "modified", false)
@@ -105,48 +116,50 @@ function replacer.save(qf_bufnr)
   end
 
   -- move/rename files
-  for index, item in pairs(original_items) do
-    local source_win_id = vim.fn.bufwinid(item.bufnr)
-    local source_is_loaded = vim.api.nvim_buf_is_loaded(item.bufnr)
-    local source_file = vim.fn.bufname(item.bufnr)
-    local source_folder = basename(source_file)
+  if rename_files then
+    for index, item in pairs(original_items) do
+      local source_win_id = vim.fn.bufwinid(item.bufnr)
+      local source_is_loaded = vim.api.nvim_buf_is_loaded(item.bufnr)
+      local source_file = vim.fn.bufname(item.bufnr)
+      local source_folder = basename(source_file)
 
-    local dest_file
+      local dest_file
 
-    -- find the destination file name
-    for part, match in pairs(vim.fn.split(changed_items[index], ":")) do
-      if part == 1 then
-        dest_file = match
-      end
-    end
-
-    -- reload source buffer
-    vim.api.nvim_buf_call(item.bufnr, function()
-      vim.cmd("edit!")
-    end)
-
-    if source_file ~= "" and source_file ~= dest_file and vim.fn.filereadable(source_file) and vim.fn.filereadable(dest_file) == 0 then
-      local dest_folder = vim.fn.mkdir(basename(dest_file), "p")
-
-      -- delete open buffer that's not visible
-      if source_is_loaded and source_win_id == -1 then
-        vim.api.nvim_buf_delete(item.bufnr, {})
+      -- find the destination file name
+      for part, match in pairs(vim.fn.split(changed_items[index], ":")) do
+        if part == 1 then
+          dest_file = match
+        end
       end
 
-      vim.fn.rename(source_file, dest_file)
+      -- reload source buffer
+      vim.api.nvim_buf_call(item.bufnr, function()
+        vim.cmd("edit!")
+      end)
 
-      -- update visible buffer to point to the new file
-      if source_win_id ~= -1 then
-        vim.api.nvim_set_current_win(source_win_id)
+      if source_file ~= "" and source_file ~= dest_file and vim.fn.filereadable(source_file) and vim.fn.filereadable(dest_file) == 0 then
+        local dest_folder = vim.fn.mkdir(basename(dest_file), "p")
 
-        vim.api.nvim_command("edit! " .. dest_file)
+        -- delete open buffer that's not visible
+        if source_is_loaded and source_win_id == -1 then
+          vim.api.nvim_buf_delete(item.bufnr, {})
+        end
 
-        vim.api.nvim_buf_delete(item.bufnr, {})
-      end
+        vim.fn.rename(source_file, dest_file)
 
-      -- delete previous folder if empty
-      if table.empty(vim.fn.readdir(source_folder)) then
-        vim.fn.delete(source_folder, "d")
+        -- update visible buffer to point to the new file
+        if source_win_id ~= -1 then
+          vim.api.nvim_set_current_win(source_win_id)
+
+          vim.api.nvim_command("edit! " .. dest_file)
+
+          vim.api.nvim_buf_delete(item.bufnr, {})
+        end
+
+        -- delete previous folder if empty
+        if table.empty(vim.fn.readdir(source_folder)) then
+          vim.fn.delete(source_folder, "d")
+        end
       end
     end
   end
